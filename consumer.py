ab051f65dc2d3a3a4b3d219e1612cf81b18b0f28
boburmirzo <boburmirzo.umurzokov@gmail.com>
2024-09-08 16:49:29 +0000

    first commit

---
+++ consumer.py
@@ -0,0 +1,19 @@
+import glassflow
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+pipeline_id = os.getenv("PIPELINE_ID")
+pipeline_access_token = os.getenv("ACCESS_TOKEN")
+
+client = glassflow.GlassFlowClient()
+pipeline_client = client.pipeline_client(pipeline_id=pipeline_id,
+                                         pipeline_access_token=pipeline_access_token)
+
+while True:
+    res = pipeline_client.consume()
+    if res.status_code == 200:
+        event_data = res.body.event
+        print(f"Data received from GlassFlow: {event_data}")
---
+++ producer.py
@@ -0,0 +1,28 @@
+import glassflow
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+pipeline_id = os.getenv("PIPELINE_ID")
+pipeline_access_token = os.getenv("ACCESS_TOKEN")
+
+
+client = glassflow.GlassFlowClient()
+pipeline_client = client.pipeline_client(
+    pipeline_id=pipeline_id, pipeline_access_token=pipeline_access_token
+)
+
+data = {
+    "name": "John Doe",
+    "email": "john.doe@example.com",
+    "phone": "123-456-7890",
+    "ssn": "123-45-6789",
+}
+
+response = pipeline_client.publish(request_body=data)
+if response.status_code == 200:
+    print(f"Data sent to GlassFlow: {data}")
+else:
+    print(f"Failed to send data: {response.text}")
